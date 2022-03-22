bb9ed7eaf5d8386fa793b2704a104bb9008e3a53
Yunkeun <yunx2@naver.com>
2022-03-22 18:46:41 +0900

    feat: 연관관계 편입 메서드 추가

--- src/main/java/com/yoonveloping/jpabook/domain/Order.java
+++ src/main/java/com/yoonveloping/jpabook/domain/Order.java
@@ -5,6 +5,7 @@ import static javax.persistence.FetchType.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -21,10 +22,12 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "orders")
-@Getter @Setter
+@Getter
+@Setter
 public class Order {
 
-	@Id @GeneratedValue
+	@Id
+	@GeneratedValue
 	@Column(name = "order_id")
 	private Long id;
 
@@ -32,16 +35,31 @@ public class Order {
 	@JoinColumn(name = "member_id")
 	private Member member;
 
-	@OneToMany(mappedBy = "order")
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
 	private List<OrderItem> orderItems = new ArrayList<>();
 
-	@OneToOne(fetch = LAZY)
+	@OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "delivery_id")
 	private Delivery delivery;
 
 	private LocalDateTime orderDate; // 주문시간
 
 	@Enumerated(EnumType.STRING)
-	private OrderStatus status;	// 주문상태 [ORDER, CANCEL]
+	private OrderStatus status;    // 주문상태 [ORDER, CANCEL]
 
+	//==연관관계 편입 메서드==//
+	public void setMember(Member member) {
+		this.member = member;
+		member.getOrders().add(this);
+	}
+
+	public void addOrderItem(OrderItem orderItem) {
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
+	}
+
+	public void setDelivery(Delivery delivery) {
+		this.delivery = delivery;
+		delivery.setOrder(this);
+	}
 }
